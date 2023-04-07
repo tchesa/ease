b03adbe8a9ab256d4a142f855020a6a059d9f754
tchesa <cesarolimpio@gmail.com>
2023-04-07 20:07:18 -0300

    add test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,25 @@ export default {
   testEnvironment: "node",
   testMatch: ["<rootDir>/src/**/*.test.ts"],
   testPathIgnorePatterns: ["/node_modules/"],
-  // coverageDirectory: "./coverage",
+  coverageDirectory: "./coverage",
   coveragePathIgnorePatterns: ["node_modules", "dist"],
   transform: {
-    "\\.ts$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+    "\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+        diagnostics: {
+          warnOnly: true,
+        },
+      },
+    ],
+  },
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
   },
 };
